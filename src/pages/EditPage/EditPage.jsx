06813fb81a2fc3ff649c2ad5eabc6f0351ea86f6
Alan Sarasate <alansarasatecc@gmail.com>
2023-09-03 15:22:03 -0300

    feat: Adcionando a funcionalidade de editar o projeto

--- src/pages/EditPage/EditPage.jsx
+++ src/pages/EditPage/EditPage.jsx
@@ -1,19 +1,24 @@
-import { useRef, useContext, useState } from "react";
+import { useRef, useContext, useState, useEffect } from "react";
 import Button from "../../components/Fragments/Button/Button";
-import TextInput from "../../components/Fragments/Input/Input";
-import { BoxWithBorder, EPWraper, EditBox, PrimaryBox, InformationsBox, EPTitle, Information, EPLabel, Box, EPSubtitle, ServicesHeader, ServicesContent } from "./EditPage.style";
+import { BoxWithBorder, EPWraper, EditBox, PrimaryBox, InformationsBox, EPTitle, Information, EPLabel, Box, EPSubtitle, ServicesHeader, ServicesContent, ServicesBox } from "./EditPage.style";
 import ProjectsContext from "../../providers/ProjectsContext";
+import ServiceCard from "../../components/ServiceCard/ServiceCard";
 
 export default function EditPage({ProjectEdit}){
-
+    
     const ProjectsDatas = useContext(ProjectsContext)
 
     const [isOpenEdit, setIsOpenEdit] = useState(false)
     const [isOpenServices, setIsOpenServices] = useState(false)
     const [editButtonName, setEditButtonName] = useState("Editar projeto")
     const [servicesButtonName, setServicesButtonName] = useState("Adicionar serviço")
-    const [inputName, setInputName] = useState("Name")
-    const [inputBudget, setInputBudget] = useState(500)
+    const [inputName, setInputName] = useState(ProjectsDatas.projects[ProjectEdit].title)
+    const [inputBudget, setInputBudget] = useState(ProjectsDatas.projects[ProjectEdit].budget)
+
+    const ServiceNameInput = useRef(null)
+    const ServiceCostInput = useRef(null)
+    const ServiceDescriptionInput = useRef(null)
+    const ProjectCategory = useRef(null)
 
     function HandleChangeName(ev){
         setInputName(ev.target.value)
@@ -32,12 +37,41 @@ export default function EditPage({ProjectEdit}){
         setServicesButtonName(servicesButtonName == "Fechar"? "Adicionar serviço" : "Fechar")
     }
 
+    const AddService = () => {
+        if(ServiceNameInput.current.value && ServiceCostInput.current.value && ServiceDescriptionInput.current.value){
+            const service = {
+                name: ServiceNameInput.current.value,
+                cost: ServiceCostInput.current.value,
+                description: ServiceDescriptionInput.current.value}
+                ProjectsDatas.projects[ProjectEdit].services.push(service)
+                ProjectsDatas.setProjects(ProjectsDatas.projects)
+                console.log(ProjectsDatas.projects[ProjectEdit].services.length)
+                console.log(ProjectsDatas.projects[ProjectEdit].services)
+        }else{
+            console.log("DEU RUIM")
+        }
+    }
+
+    function removeService(id){
+        ProjectsDatas.projects[ProjectEdit].services.filter((service, index)=>(
+            index !== id 
+            ))
+    }
+
+    const ModifyProject = () => {
+        ProjectsDatas.projects[ProjectEdit].title = inputName
+        ProjectsDatas.projects[ProjectEdit].budget = inputBudget
+        ProjectsDatas.projects[ProjectEdit].category = ProjectCategory.current.value
+        setIsOpenEdit(!isOpenEdit)
+        setEditButtonName(editButtonName == "Fechar"? "Editar projeto" : "Fechar")
+    }
+ 
     return(
         <EPWraper>
             <BoxWithBorder>
                 <PrimaryBox>
                     <InformationsBox>
-                        <EPTitle>TESTE</EPTitle>
+                        <EPTitle>{ProjectsDatas.projects[ProjectEdit].title}</EPTitle>
                     </InformationsBox>
                     <Button buttonText={editButtonName} handleClickFunction={EditHandleClick}/>
                 </PrimaryBox>
@@ -48,30 +82,45 @@ export default function EditPage({ProjectEdit}){
                         <EPLabel>Orçamento do projeto: </EPLabel>
                         <input type="number" placeholder="Number" value={inputBudget} onChange={HandleChangeBudget}/>
                         <EPLabel>Nome do projeto: </EPLabel>
-                        <select defaultValue={"Infra"}>
+                        <select defaultValue={ProjectsDatas.projects[ProjectEdit].category} ref={ProjectCategory}>
                             <option value="Infra">Infra</option>
                             <option value="Desenvolvimento">Desenvolvimento</option>
                             <option value="Design">Design</option>
                             <option value="Planejamento">Planejamento</option>
                         </select>
-                        <Button buttonText={"Concluir edição"}/>
+                        <Button buttonText={"Concluir edição"} handleClickFunction={ModifyProject}/>
                     </EditBox> :
                     <Box>
-                        <Information>Categoria: <span>teste</span></Information>
-                        <Information>Total do orçamento: <span>teste</span></Information>
-                        <Information>Total utilizado: <span>teste</span></Information>
-                    </Box>
-                    }
+                        <Information>Categoria: <span>{ProjectsDatas.projects[ProjectEdit].category}</span></Information>
+                        <Information>Total do orçamento: <span>{ProjectsDatas.projects[ProjectEdit].budget}</span></Information>
+                        <Information>Total utilizado: <span>{ProjectsDatas.projects[ProjectEdit].budget}</span></Information>
+                    </Box>}
             </BoxWithBorder>
             <BoxWithBorder>
                 <ServicesHeader>
                     <EPSubtitle>Adicionar Serviços</EPSubtitle>
                     <Button buttonText={servicesButtonName} handleClickFunction={ServicesHandleClick}/>
                 </ServicesHeader>
+                {isOpenServices &&
+                    <EditBox>
+                        <EPLabel>Nome do serviço: </EPLabel>
+                        <input type="text" placeholder="Nome do serviço" ref={ServiceNameInput}/>
+                        <EPLabel>Custo do serviço: </EPLabel>
+                        <input type="number" placeholder="Custo do serviço" ref={ServiceCostInput}/>
+                        <EPLabel>Descrição do serviço: </EPLabel>
+                        <input type="text" placeholder="Descrição do serviço" ref={ServiceDescriptionInput}/>
+                        <Button buttonText={"Adicionar serviço"} handleClickFunction={AddService}/> 
+                    </EditBox>}
             </BoxWithBorder>
             <ServicesContent>
                 <EPSubtitle>Serviços</EPSubtitle>
-                <span>Não há serviços cadastrados!</span>
+                {ProjectsDatas.projects[ProjectEdit].services.length ? 
+                    <ServicesBox>
+                        {ProjectsDatas.projects[ProjectEdit].services.map((service, index) => (
+                            <ServiceCard Title={service.name} Cost={service.cost} Description={service.description} key={index} removeFunction={() => removeService(index)}/>
+                        ))}
+                    </ServicesBox> : 
+                    <span>Não há serviços cadastrados!</span>}
             </ServicesContent>
         </EPWraper>
     )
